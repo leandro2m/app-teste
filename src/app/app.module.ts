d15f755aaacbe1b89806ddfccf03fcb5a9635a1e
leandro2m <lmmoraes@gmail.com>
2019-04-19 18:14:31 -0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
+import { HomePageComponent } from './page/home-page/home-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  providers: [NbSidebarService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
